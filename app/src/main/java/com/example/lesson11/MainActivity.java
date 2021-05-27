6621e8ec393b3111946fc86f68c1918246ea2e57
andrey_sirak <andrey.sirak97@gmail.com>
2021-05-27 16:07:24 +0300

    Init Commit Commit 1

--- app/src/main/java/com/example/lesson11/MainActivity.java
+++ app/src/main/java/com/example/lesson11/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lesson11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void switchLayout(View view) {
+        setContentView(R.layout.activity_edit_texts);
+    }
+
+    public void switchLayout2(View view) {
+        setContentView(R.layout.activity_calendar_view);
     }
 }
